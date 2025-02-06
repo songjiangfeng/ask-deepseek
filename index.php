cf28c469566a025f87df25d8e04331320a8715b7
songjiangfeng <819131329@qq.com>
2025-02-06 23:41:51 +0800

    ask deepseek change from local to  the API

--- index.php
+++ index.php
@@ -13,15 +13,20 @@
 
     <?php
 
-            set_time_limit(0);
-    $t1 = microtime(true);
+    set_time_limit(0);
+
     require './vendor/autoload.php';
+    use Noodlehaus\Config;
+    use Noodlehaus\Parser\Yaml;
+    $conf = new Config('api.yaml',new Yaml);
 
+    $deepseek = $conf->get('deepseek');
+    $siliconflow = $conf->get('siliconflow');
 
-    $toop =  top100();
+    $toop =  top100("siliconflow:");
     if (!isset($_GET['ask'])) {
         foreach ($toop as $key) {
-            $title = str_replace('deepseek:', '', $key);
+            $title = str_replace('siliconflow:', '', $key);
 
             print '<a class="text-sky-600" href="/index.php?ask='.$title.'">'.$title.'</a><br/>';
 
@@ -31,12 +36,13 @@
         $ask = $_GET['ask'] ?? '你好';
         $prompt = $_GET['comment'] ?? $ask;
         echo "<a class='text-sky-600' href=./index.php>返回</a>";
-        echo ask_deepseek($ask, $prompt);
-
+        $t1 = microtime(true);
+        
+        echo ask_deepseek($ask, $prompt,$deepseek);
+        $t2 = microtime(true);
+        echo '<p style="color:red">耗时'.round($t2 - $t1, 3).'秒<br>内存消耗: ' . round(memory_get_usage() / 1024 / 1024, 3).'mb<br/></p>';
     }
 
-    $t2 = microtime(true);
-    echo '<p style="color:red">耗时'.round($t2 - $t1, 3).'秒<br>内存消耗: ' . round(memory_get_usage() / 1024 / 1024, 3).'mb<br/></p>';
     ?>
     </div>
     </div>
@@ -59,71 +65,128 @@ function top100($prefix = 'deepseek:')
     return $top[1];
 }
 
-    function ask_redis($key, $prefix = 'deepseek:')
-    {
-        $client = new Predis\Client([
-            'scheme' => 'tcp',
-            'host'   => '127.0.0.1',
-            'port'   => 6379,
-        ]);
-        return $client->get($prefix.$key);
-    }
+function ask_deepseek_siliconflow($ask, $prompt, $config)
+{
 
+    $html = '';
+    $client = new Predis\Client([
+        'scheme' => 'tcp',
+        'host'   => '127.0.0.1',
+        'port'   => 6379,
+    ]);
 
-    function ask_deepseek($ask, $prompt, $prefix = 'deepseek:')
-    {
+    $apiKey = $config['apiKey'];
+    $url = $config['url'];
+    $model = $config['model'];
+    $prefix = $config['prefix'];
+    $headerArray = array("Authorization: Bearer ".$apiKey."","Content-type:application/json");
 
-        $html = '';
-        $client = new Predis\Client([
-            'scheme' => 'tcp',
-            'host'   => '127.0.0.1',
-            'port'   => 6379,
-        ]);
+    $data = [
+        'model' => $model,
+        'messages' => array(["role"=>"user","content"=>$prompt]),
+        "stream"=>False,
+        "max_tokens"=> 512,
+        "stop"=>["null"],
+        "temperature"=> 0.7,
+        "top_p"=> 0.7,
+        "top_k"=> 50,
+        "frequency_penalty"=> 0.5,
+        "n"=> 1,
+        "response_format"=> ["type"=> "text"],
+    
+    ];
 
-        $apiKey = 'your-api-key-here';
-        $url = 'http://localhost:11434/api/generate';
+    $key = $prefix.$ask;
 
-        $model = 'deepseek-r1:1.5b';
-        $headerArray = array("Content-type:application/json;charset='utf-8'","Accept:application/json");
-        $data = [
-            'model' => $model,
-            'prompt' => $prompt,
-            'stream' => false
-        ];
+    if ($client->exists($key)) {
+        $html = $client->get($key);
 
-        $key = $prefix.$ask;
+    } else {
 
-        if ($client->exists($key)) {
-            $html = $client->get($key);
+        try {
 
-        } else {
+            $data  = json_encode($data);
 
-            try {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($curl);
+            curl_close($curl);
+            $res  = json_decode($output, true) ;
 
-                $data  = json_encode($data);
-                $curl = curl_init();
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-                curl_setopt($curl, CURLOPT_POST, 1);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-                $output = curl_exec($curl);
-                curl_close($curl);
-                $res  = json_decode($output, true) ;
-
-                $regex = '/<think>(.*?)<\/think>/s';
-                $text = $res['response'];
-                // 使用正则表达式匹配并提取 <think> 和 </think> 之间的内容
-                $cleanText = preg_replace($regex, '', $text);
-                $Parsedown = new Parsedown();
-                $html = $Parsedown->text($cleanText);
-                $client->set($key, $html);
-            } catch (Exception $e) {
-                echo 'Error: ' . $e->getMessage();
-            }
-
+            $content =  $res['choices'][0]['message']['content'];
+            $Parsedown = new Parsedown();
+            $html = $Parsedown->text( $content);
+        
+            $client->set($key, $html);
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
         }
-        return $html;
+
     }
+    return $html;
+}
+
+
+
+function ask_deepseek($ask, $prompt, $config)
+{
+
+    $html = '';
+    $client = new Predis\Client([
+        'scheme' => 'tcp',
+        'host'   => '127.0.0.1',
+        'port'   => 6379,
+    ]);
+
+    $apiKey = $config['apiKey'];
+    $url = $config['url'];
+    $model = $config['model'];
+    $prefix = $config['prefix'];
+    $headerArray = array("Authorization: Bearer ".$apiKey."","Content-type:application/json");
+    $data = [
+        
+        'model' => $model,
+        'messages' => array(["role"=>"user","content"=>$prompt]),
+        "stream"=>False
+    ];
+
+    $key = $prefix.$ask;
+
+    if ($client->exists($key)) {
+        $html = $client->get($key);
+
+    } else {
+
+        try {
+
+            $data  = json_encode($data);
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($curl);
+            curl_close($curl);
+            $res  = json_decode($output, true) ;
+
+            $content =  $res['choices'][0]['message']['content'];
+            
+            $Parsedown = new Parsedown();
+            $html = $Parsedown->text( $content);
+            $client->set($key, $html);
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+
+    }
+    return $html;
+}
